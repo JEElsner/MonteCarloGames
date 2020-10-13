8c73cf1b16ac13f9ab58f403af38db6470f75185
Jonathan Elsner <jelsnerbusiness@outlook.com>
2020-10-12 23:32:10 -0700

    Implement Tic-Tac-Toe as a GameState

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,9 @@
 import numpy as np
 import os
 
+from game import GameState
+from vectorize_objects import vectorize
+
 import monte_carlo_ttt as mct
 from monte_carlo_ttt import Node
 
@@ -14,6 +17,120 @@ COLOR = {
 
 sides = {1: COLOR['YELLOW'] + 'X' + COLOR['ENDC'],
          -1: COLOR['GREEN'] + 'O' + COLOR['ENDC']}
+
+
+class TicTacToe(GameState):
+    def __init__(self, board=None, turn=None):
+        self.players = ['X', 'O']
+
+        if board is None:
+            # If starting a new game
+            shape = np.array([3, 3])
+
+            self.board = np.arange(np.product(shape)).reshape(
+                shape).astype(dtype=str)
+        elif np.all(np.array(board.shape) == board.shape[1:]):
+            # If continuing a game, and the board is properly configured
+            self.board = board
+        else:
+            raise ValueError('Invalid initializing board size')
+
+        # If we have a custom starting player
+        if turn is not None:
+            # Make sure the current turn is valid
+            if turn in self.players:
+                # Loop until we get to the current player's turn
+                while self.players[0] != turn:
+                    self.players.append(self.players.pop(0))
+            else:
+                raise ValueError('Invalid initializing player')
+
+        # Vectorize the TicTacToe class
+        vectorize(TicTacToe)
+
+    def get_state(self):
+        '''
+        Returns the state of the game board
+        '''
+        return self.board
+
+    def get_current_turn(self):
+        '''
+        Returns the player whose turn it currently is. (Players are 'X' and 'O')
+        '''
+        return self.players[0]
+
+    def get_possible_moves(self, player):
+        '''
+        Return the possible moves as a list of integers as the index of each
+        square in the Tic-Tac-Toe grid.
+        '''
+        return np.arange(self.board.size)[np.char.isdigit(self.board.flat)]
+
+    def move(self, player, move):
+        '''
+        Returns a new TicTacToe object representing the next state of the game
+        after this move is made.
+
+        Arguments:
+
+            player      The player who is making this move
+
+            move        The integer representing the index of the square to
+                        place the player's piece
+        '''
+
+        # Make sure the player can play
+        if player != self.get_current_turn():
+            raise ValueError(
+                type(player), 'It is not {0}\'s turn'.format(player))
+
+        # Copy the state to create a new TicTacToe state
+        new_state = np.copy(self.board)
+        new_state.flat[move] = player
+
+        # Create and return the next game state
+        return TicTacToe(board=new_state, turn=self.players[0])
+
+    def get_winner(self):
+        '''
+        Return the player who has won in the current game state, or None if no
+        player has won yet
+        '''
+
+        # Check the rows for three in a row
+        for row in self.board:
+            if np.all(row == row[0]) and row[0] != 0:
+                return row[0]
+
+        # Check the columns for three in a row
+        for col in self.board.T:
+            if np.all(col == col[0]) and col[0] != 0:
+                return col[0]
+
+        # Check the NW-SE diagonal for three in a row
+        if np.all(self.board.diagonal() == self.board.diagonal()[0]) and self.board.diagonal()[0] != 0:
+            return self.board.diagonal()[0]
+
+        # Check the NE-SW diagonal for three in a row
+        flipped_diagonal = np.fliplr(self.board).diagonal()
+        if np.all(flipped_diagonal == flipped_diagonal[0]) and flipped_diagonal[0] != 0:
+            return flipped_diagonal[0]
+
+        # Return None since there is not yet a winner
+        return None
+
+    def __str__(self):
+        '''
+        Returns a human-readable string representation of the game state
+        '''
+
+        string = ''
+
+        for v, row in enumerate(self.board):
+            string += ' ' + ' | '.join(row) + '\n'
+            if v != len(self.board)-1:
+                string += '+'.join([('-' * 3) for i in row])
 
 
 def print_board(board):
--- ttt_test.py
+++ ttt_test.py
@@ -4,32 +4,86 @@ import numpy as np
 import tic_tac_toe as ttt
 
 
-class WinTest(unittest.TestCase):
+class TTTTest(unittest.TestCase):
+    def test_initialization(self):
+        state = ttt.TicTacToe()
+
+        expected_state = np.arange(9).reshape((3, 3)).astype(dtype=str)
+        self.assertTrue(np.all(state.get_state() == expected_state),
+                        'Initialization and State')
+
+    def test_bad_init_state(self):
+        def construct(): return ttt.TicTacToe(board=np.zeros((3, 2)))
+
+        self.assertRaises(ValueError, construct)
+
+    def test_bad_init_player(self):
+        def construct(): return ttt.TicTacToe(turn='asdf')
+
+        self.assertRaises(ValueError, construct)
+
+    def test_get_curr_player(self):
+        self.assertEqual(ttt.TicTacToe().get_current_turn(), 'X')
+
+    def test_get_moves(self):
+        state = ttt.TicTacToe()
+        expected_moves = np.arange(9)
+
+        self.assertTrue(np.all(state.get_possible_moves('X') ==
+                               expected_moves), 'Empty board move possibilities')
+
+        state.board.flat[8] = 'O'
+        self.assertTrue(np.all(state.get_possible_moves('X') ==
+                               expected_moves[:-1]), 'Modified board move possibilites')
+
+    def test_move(self):
+        state1 = ttt.TicTacToe()
+
+        state2 = state1.move('X', 0)
+
+        expected_state = np.arange(9).reshape(3, 3).astype(dtype=str)
+        expected_state.flat[0] = 'X'
+
+        self.assertTrue(np.all(state2.get_state() ==
+                               expected_state), 'State after move')
+        self.assertNotEqual(state2, state1, 'Move copy')
+        self.assertFalse(np.all(state2.board == state1.board),
+                         'Move copy board')
+        self.assertFalse(np.all(state1.board == expected_state),
+                         'Move immutability')
+
+    def test_wrong_turn(self):
+        game = ttt.TicTacToe()
+
+        def move_boi(): game.move('O', 0)
+
+        self.assertRaises(ValueError, move_boi)
+
     def test_win_states(self):
-        for i in range(3):
-            b = np.zeros((3, 3))
-            b[i] += 1
+        self.assertIsNone(ttt.TicTacToe().get_winner(), 'No winner')
 
-            win, winner = ttt.is_win_state(b)
-            self.assertTrue(win, msg=i)
+        for player in ['X', 'O']:
+            for i in range(3):
+                b = np.arange(9).reshape(3, 3).astype(dtype=str)
+                b[i] = player
 
-            t_win, t_winner = ttt.is_win_state(b.T)
-            self.assertTrue(t_win, msg=i)
+                game = ttt.TicTacToe(board=b)
+                self.assertEqual(game.get_winner(), player,
+                                 'Win: {p} Row: {i}'.format(p=player, i=i))
 
-        b = np.identity(3)
-        diag_win, _ = ttt.is_win_state(b)
-        self.assertTrue(diag_win)
+                game = ttt.TicTacToe(board=b.T)
+                self.assertEqual(game.get_winner(), player,
+                                 'Win: {p} Col: {i}'.format(p=player, i=i))
 
-        b = np.fliplr(b)
-        diag2_win, _ = ttt.is_win_state(b)
-        self.assertTrue(diag2_win)
+            b = np.arange(9).reshape(3, 3).astype(dtype=str)
+            b[np.identity(3, dtype=bool)] = player
+            game = ttt.TicTacToe(board=b)
+            self.assertEqual(game.get_winner(), player,
+                             'Win: {0} NW-SE Diag'.format(player))
 
-    def test_negative_win(self):
-        b = np.zeros((3, 3))
-        b[0] += -1
-
-        win, winner = ttt.is_win_state(b)
-        self.assertTrue(winner, -1)
+            b = np.fliplr(b)
+            self.assertEqual(game.get_winner(), player,
+                             'Win: {0} NE-SW Diag'.format(player))
 
 
 if __name__ == '__main__':
