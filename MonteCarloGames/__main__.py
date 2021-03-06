6ce19987a2217007b094b3d7bafb915af52833cd
Jonathan Elsner <jelsnerbusiness@outlook.com>
2021-03-06 14:24:29 -0800

    Implement game saving

--- MonteCarloGames/__main__.py
+++ MonteCarloGames/__main__.py
@@ -1,12 +1,16 @@
 import ConsoleQuestionPrompts as questions
+import datetime
+import pickle
 
 from . import *
+
+file_extension = '.pickle'
 
 
 def main():
     games = {'Tic Tac Toe': TicTacToe, 'Othello': Othello}
 
-    choice = questions.option_question(
+    choice: type = questions.option_question(
         'Which game would you like to play?', games.keys(), list(games.values()))
 
     game_types = ['All AI', 'Player vs AI', 'Player vs Player']
@@ -21,8 +25,11 @@ def main():
 
     played_game = MonteCarloTree(choice, players)
 
+    rounds = [(0, played_game.current_state)]
     print(played_game.current_state)
+
     for move, state in played_game.play_rounds():
+        rounds.append((move, state))
         print(state)
 
         if state.is_finished():
@@ -30,6 +37,16 @@ def main():
                 print(state.get_winner(), 'wins!')
             else:
                 print('The game is a draw!')
+
+    if questions.yes_no_question('Save this game? '):
+        name = questions.ask_question('Game name: ')
+
+        filename = '-'.join([choice.__name__, name,
+                             datetime.datetime.now().strftime('%Y_%m_%d_%H%M')]) + file_extension
+
+        with open(filename, mode='wb') as file:
+            pickle.dump(rounds, file)
+            print('Game saved as', filename)
 
 
 if __name__ == '__main__':
